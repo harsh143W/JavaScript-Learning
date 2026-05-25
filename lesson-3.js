d9cb40cde3bd5f68186e7e59cef3f81d05178bc5
Harsh <harshvankar631@gmail.com>
2026-05-25 12:32:19 +0530

    Add lesson-3.js with JavaScript examples

---
+++ lesson-3.js
@@ -0,0 +1,57 @@
+'hello' 
+ //Ourpur 'hello'
+
+alert('Hello'); 
+undefined
+
+'some' + 'text' 
+//OutPUt 'sometext'
+
+'some' + 'more' + 'text' 
+//Output 'somemoretext'
+
+typeof 2 
+//Outut 'number'
+
+typeof 'H' 
+//Output 'string'
+
+'Hello' +  2 
+//Output'Hello2'
+
+'$' + 20.95 + 7.99 
+//Output '$20.957.99'
+
+'$' + (20.95 + 7.99) 
+//Output '$28.939999999999998'
+
+'$' + Math.round(20.95 + 7.99) 
+//Output '$29'
+
+'$' + Math.round((2095+799))/100;
+//Output '$28.94'
+
+'Items (' + (1+1) + '): $' + (2095 + 799)/100 
+//Output 'Items (2): $28.94'
+
+alert('Items (' + (1+1) + '): $' + (2095 + 799)/100);
+undefined
+
+"I'm  learning  JavaScript"
+//Output "I'm  learning  JavaScript"
+
+'I\'m learning JavaScript'
+//Output "I'm learning JavaScript"
+
+alert('Some\ntext');
+undefined
+  
+`hello`
+//Output 'hello'
+  
+`Item (${1 + 1}): $${(2095 + 799)/100}`
+//Output 'Item (2): $28.94'
+  
+`some
+text ` 
+//Output 'some\ntext '
