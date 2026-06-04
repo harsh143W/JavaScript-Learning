500fbb9b1f66b0c9e5b57d9eea502c3107f85e01
Harsh <harshvankar631@gmail.com>
2026-06-04 15:04:41 +0530

    Add sum of two numbers functionality

---
+++ sumoftwonumber.js
@@ -0,0 +1,7 @@
+let firstNum = prompt("What is frist Number:-");
+
+    let secondNum = prompt("What is second Number:-");
+
+    let sum = Number(firstNum) + Number(secondNum);
+
+    alert(`firstNum:- ${firstNum},secondNum:- ${secondNum},Sum of the Number:- ${sum}`);
